40de2c22945872945f7d4c3310f2e7b32848af13
slagrabos <sla.grabos@gmail.com>
2021-06-25 12:42:53 +0200

    added axios for create, edit and delete

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { SyntheticEvent, useState } from "react";
 import { act } from "react-dom/test-utils";
 import { Button, Item, Label, Segment } from "semantic-ui-react";
 import { Activity } from "../../../app/Models/activity";
@@ -6,10 +6,18 @@ interface Props {
   activities: Activity[];
   selectActivity: (id: string) => void;
   deleteActivity: (id:string) => void
+  submitting: boolean;
 
 }
 
-export default function ActivityList({activities, selectActivity, deleteActivity} :Props) {
+export default function ActivityList({activities, selectActivity, deleteActivity, submitting} :Props) {
+
+  const [target, setTarget] = useState('');
+  function handleActivityDelete(event: SyntheticEvent<HTMLButtonElement> , id:string) {
+
+    setTarget(event.currentTarget.name);
+    deleteActivity(id);
+  }
   return (
     <Segment>
       <Item.Group divided>
@@ -35,7 +43,10 @@ export default function ActivityList({activities, selectActivity, deleteActivity
 
                   </Button>
 
-                  <Button onClick={() => deleteActivity(activity.id)}  floated='right' content='Delete' color='red'>
+                  <Button 
+                  name={activity.id}
+                  loading={submitting && target=== activity.id} onClick={(event) =>
+                     handleActivityDelete(event, activity.id)}  floated='right' content='Delete' color='red'>
 
 </Button>
                   <Label basic content={activity.category}/>
